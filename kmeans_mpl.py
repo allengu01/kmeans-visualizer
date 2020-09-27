fb96ce09d9f26358173e598db0847f600142704d
allengu01 <allen.gu01@gmail.com>
2020-09-27 14:09:46 -0500

    Added gifs

--- kmeans_mpl.py
+++ kmeans_mpl.py
@@ -1,29 +1,10 @@
-import os, shutil
 import numpy as np
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import plotly.tools as tls
-import plotly.express as px
-import cv2
 
 # READ IMAGE
-img = cv2.imread('images/minimalist_landscape1.jpg')
-resized_img = cv2.resize(img, (30, 30))
-
-def split_rgb(image):
-    b, g, r = cv2.split(image)
-    return r, g, b
-
-def flatten(arr):
-    n = arr.size
-    return arr.reshape([n,])
-
-img_r, img_g, img_b = split_rgb(resized_img)
-flatten_img_r, flatten_img_g, flatten_img_b = list(map(flatten, [img_r, img_g, img_b]))
-pixels = np.stack([flatten_img_r, flatten_img_g, flatten_img_b], axis=1)
-
 def distance(p1, p2):
     """
     Returns the Euclidean distance
@@ -76,37 +57,35 @@ def update_centroids(data, centroids, clusters):
     return centroids, change
 
 def k_means(data, k, directory_name):
-    reset()
-
     centroids = initialize_random_centroids(data, k)
     clusters = cluster_data(data, centroids)
-    plot_rgb(data, centroids, "Initialize Random Centroids", directory_name + 'iteration_start' + '.png')
 
 
     centroids_changed = True
-    i = 1
+    i = 0
+    iteration_files = []
     while centroids_changed:
         print("Iteration:", i)
+        plot_filename = 'iteration_' + str(i) + '.png'
+        plot_rgb(data, centroids, i, directory_name + plot_filename)
+
         clusters = cluster_data(data, centroids)
         centroids, centroids_changed = update_centroids(data, centroids, clusters)
-        plot_rgb(data, centroids, i, directory_name + 'iteration_' + str(i) + '.png')
-
+        iteration_files.append(plot_filename)
         i += 1
     print("Cost:", cost_function(data, centroids, clusters))
-    return centroids
+    return centroids, iteration_files
 
-def plot_rgb(data, centroids, iteration, file_path, end = False):
+def plot_rgb(data, centroids, i, file_path, end = False):
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111, projection='3d')
-    ax.set_xlabel("Red")
-    ax.set_ylabel("Green")
-    ax.set_zlabel("Blue")
-    ax.set_title("Iteration: " + str(iteration))
+    ax.set_title("Iteration: " + str(i))
 
     ax.xaxis.labelpad, ax.yaxis.labelpad, ax.zaxis.labelpad = 10, 10, 10
     ax.title.set_position([0.85, 1])
     ax.title.set_size(10) 
 
+    ax.set_axis_off()
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_zticks([])
@@ -118,23 +97,51 @@ def plot_rgb(data, centroids, iteration, file_path, end = False):
 
     for r, g, b in centroids:
         ax.scatter3D(r, g, b, s=200, facecolor=(r/255, g/255, b/255), edgecolor="black", zorder=2)
-    fig.savefig(file_path, dpi=300)
-
-    if end:
-        pass
-        plt.show()
-    else:
-        fig.close()
+    fig.savefig(file_path, dpi=100)
 
     return fig, ax   
 
-def reset():
-    shutil.rmtree("figs")
-    os.mkdir('figs')
+def rotate(data, centroids, directory_name):
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection='3d')
     
-def run_kmeans():
-    reset()
+    ax.xaxis.labelpad, ax.yaxis.labelpad, ax.zaxis.labelpad = 10, 10, 10
+    ax.title.set_position([0.85, 1])
+    ax.title.set_size(10) 
 
-    centroids_result = k_means(pixels, 4, 'figs/')
-    final_plot, final_ax = plot_rgb(pixels, centroids_result, "Final", 'figs/iteration_final.png', True)
+    ax.set_axis_off()
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_zticks([])
 
+    r, g, b = data[:, 0], data[:, 1], data[:, 2]
+    point_opacity = 0.7
+    point_edge_color = np.hstack([data/255, np.ones([data.shape[0], 1]) * point_opacity]) # sets color of each point to the pixel value with point_opacity
+    ax.scatter3D(r, g, b, edgecolor=point_edge_color, facecolor=np.zeros([data.shape[0], 4]), zorder=1) # facecolor = transparent
+
+    for r, g, b in centroids:
+        ax.scatter3D(r, g, b, s=200, facecolor=(r/255, g/255, b/255), edgecolor="black", zorder=2)
+    
+    rotate_filenames = []
+    for angle in range(0, 360, 3):
+        ax.set_title("Iteration: Final")
+
+        ax.view_init(elev=30, azim=angle)
+        cur_filename = "rotate_animation" + str(angle) + ".png"
+        fig.savefig(directory_name + cur_filename, dpi=100)
+        rotate_filenames.append(cur_filename)
+    return rotate_filenames
+    
+def run_kmeans(data, k):
+    filenames = {}
+
+    centroids_result, iteration_files = k_means(data, k, 'figs/')
+    final_plot, final_ax = plot_rgb(data, centroids_result, "Final", 'figs/iteration_final.png', True)
+    iteration_files.append('iteration_final.png')
+    filenames['iterate'] = iteration_files
+
+    rotate_files = rotate(data, centroids_result, 'figs/')
+    filenames['rotate'] = rotate_files
+    plt.close("all")
+
+    return filenames
